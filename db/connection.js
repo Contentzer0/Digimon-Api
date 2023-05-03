49a1ec1979c95e3be8faa1f1312b14c0a89979fb
Conzer <119615637+Contentzer0@users.noreply.github.com>
2023-05-03 11:46:26 -0400

    ya ugly

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
-const MONGODB_URI =  "mongodb://127.0.0.1:27017/digi-api"
+const MONGODB_URI = "mongodb+srv://Conykins:GdufoYMMkj9hJSPp@cluster0.aagb6gk.mongodb.net/?retryWrites=true&w=majority"|| "mongodb://127.0.0.1:27017/digi-api"
 // this is for Model.findByIdAndUpdate method, specifically so that {new: true}
 mongoose.set("returnOriginal", false)
 mongoose.connect(MONGODB_URI).catch((error) => 
